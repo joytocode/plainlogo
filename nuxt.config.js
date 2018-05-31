f75bb2725c35b3fc13b341f20157c2d5df5b092a
Bao D <30614076+baocancode@users.noreply.github.com>
2018-05-31 22:21:21 +1000

    feat: add favicon and meta (#14)

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,9 +16,20 @@ exports.loading = {
 
 exports.head = {
   meta: [
-    { charset: 'utf-8' }
+    { charset: 'utf-8' },
+    { name: 'mobile-web-app-capable', content: 'yes' },
+    { name: 'apple-mobile-web-app-title', content: 'PlainLogo' },
+    { name: 'application-name', content: 'PlainLogo' },
+    { name: 'msapplication-TileColor', content: '#ffffff' },
+    { name: 'theme-color', content: '#ffffff' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui' }
   ],
   link: [
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+    { rel: 'manifest', href: '/site.webmanifest' },
+    { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
     { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' },
     { rel: 'stylesheet', type: 'text/css', href: 'https://use.fontawesome.com/releases/v5.0.13/css/all.css', integrity: 'sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp', crossorigin: 'anonymous' }
   ]
@@ -30,7 +41,6 @@ exports.css = [
 
 exports.plugins = [
   path.join(__dirname, 'src/plugins/vuetify'),
-  path.join(__dirname, 'src/plugins/vee-validate'),
   path.join(__dirname, 'src/plugins/vue-async-computed'),
   { src: path.join(__dirname, 'src/plugins/blob'), ssr: false }
 ]
@@ -44,7 +54,7 @@ exports.build = {
   vendor: [
     // Only keep common vendor modules here
     'axios', 'blob-polyfill', 'blueimp-canvas-to-blob', 'change-case', 'color-string', 'file-saver', 'js-base64',
-    'lodash.debounce', 'opentype.js', 'qs', 'vee-validate', 'vue-async-computed', 'vue-color', 'vuetify'
+    'lodash.debounce', 'opentype.js', 'qs', 'vue-async-computed', 'vue-color', 'vuetify'
   ],
   extend (config) {
     // inline >= 2 will cause errors https://github.com/mishoo/UglifyJS2/issues/2842
--- src/plugins/vee-validate.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import VeeValidate from 'vee-validate'
-
-Vue.use(VeeValidate, { events: 'blur', inject: false })
