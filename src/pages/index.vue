75a4ecda4f1635c5716b7a809473a2e3db9bf394
Bao D <30614076+baocancode@users.noreply.github.com>
2018-06-04 23:03:21 +1000

    feat: update urls and refer to joytocode (#18)

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -123,6 +123,35 @@
         @params-change="paramsListener"
       />
     </v-container>
+    <div class="text-xs-center my-4">
+      <p class="body-2">A product by <a
+        href="https://www.joytocode.com/"
+        target="_blank"
+      >JoyToCode</a>.</p>
+      <v-btn
+        href="https://github.com/joytocode"
+        target="_blank"
+        title="GitHub"
+        icon
+      >
+        <v-icon>fab fa-github</v-icon>
+      </v-btn>
+      <v-btn
+        href="https://twitter.com/joytocode"
+        target="_blank"
+        title="Twitter"
+        icon
+      >
+        <v-icon>fab fa-twitter</v-icon>
+      </v-btn>
+      <v-btn
+        href="mailto:team@joytocode.com"
+        title="Email"
+        icon
+      >
+        <v-icon>fas fa-envelope</v-icon>
+      </v-btn>
+    </div>
   </div>
 </template>
 
@@ -157,7 +186,7 @@ export default {
   },
   computed: {
     url () {
-      return `https://joytocode.github.io/plainlogo${this.$route.fullPath}`
+      return `https://plainlogo.joytocode.com${this.$route.fullPath}`
     }
   },
   created () {
